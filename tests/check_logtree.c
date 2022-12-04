654f3375c36699af43324704f3d12226c6340eda
taldoflemis <coderflemis@gmail.com>
2022-12-04 16:58:18 -0300

    ref: log_tree will use time as key

--- tests/check_logtree.c
+++ tests/check_logtree.c
@@ -15,21 +15,21 @@ END_TEST
 
 START_TEST(test_registrar) {
   RBT *T = log_inicializar();
-  log_registrar(T, 1, Premium, 33, 1);
-  log_registrar(T, 2, Ouro, 34, 2);
-  log_registrar(T, 3, Prata, 35, 3);
-  ck_assert(T->raiz->chave == Ouro);
-  ck_assert(T->raiz->esq->chave == Premium);
-  ck_assert(T->raiz->dir->chave == Prata);
+  log_registrar(T, 1, Premium, 33, 1, 1);
+  log_registrar(T, 2, Ouro, 34, 2, 1);
+  log_registrar(T, 3, Prata, 35, 3, 1);
+  ck_assert(T->raiz->chave == 34);
+  ck_assert(T->raiz->esq->chave == 33);
+  ck_assert(T->raiz->dir->chave == 35);
 }
 END_TEST
 
 START_TEST(test_obter_soma_por_classe) {
   RBT *T = log_inicializar();
-  log_registrar(T, 1, Premium, 10, 1);
-  log_registrar(T, 2, Premium, 20, 2);
-  log_registrar(T, 3, Premium, 30, 3);
-  log_registrar(T, 4, Premium, 40, 3);
+  log_registrar(T, 1, Premium, 10, 1, 1);
+  log_registrar(T, 2, Premium, 20, 2, 1);
+  log_registrar(T, 3, Premium, 30, 3, 1);
+  log_registrar(T, 4, Premium, 40, 3, 1);
   ck_assert(log_obter_soma_por_classe(T, T->raiz, Premium) == 100);
   ck_assert(log_obter_soma_por_classe(T, T->raiz, Ouro) == 0);
 }
@@ -37,12 +37,12 @@ END_TEST
 
 START_TEST(test_obter_contagem_por_classe) {
   RBT *T = log_inicializar();
-  log_registrar(T, 1, Premium, 10, 1);
-  log_registrar(T, 2, Premium, 20, 2);
-  log_registrar(T, 3, Premium, 30, 3);
-  log_registrar(T, 4, Premium, 40, 3);
-  log_registrar(T, 5, Premium, 40, 3);
-  log_registrar(T, 6, Premium, 40, 3);
+  log_registrar(T, 1, Premium, 10, 1, 1);
+  log_registrar(T, 2, Premium, 20, 2, 1);
+  log_registrar(T, 3, Premium, 30, 3, 1);
+  log_registrar(T, 4, Premium, 40, 3, 1);
+  log_registrar(T, 5, Premium, 40, 3, 1);
+  log_registrar(T, 6, Premium, 40, 3, 1);
   ck_assert(log_obter_contagem_por_classe(T, T->raiz, Premium) == 6);
   ck_assert(log_obter_contagem_por_classe(T, T->raiz, Ouro) == 0);
 }
@@ -50,12 +50,12 @@ END_TEST
 
 START_TEST(test_media_por_classe) {
   RBT *T = log_inicializar();
-  log_registrar(T, 1, Premium, 10, 1);
-  log_registrar(T, 2, Premium, 20, 2);
-  log_registrar(T, 3, Premium, 30, 3);
-  log_registrar(T, 4, Premium, 40, 3);
-  log_registrar(T, 5, Premium, 50, 3);
-  log_registrar(T, 6, Premium, 60, 3);
+  log_registrar(T, 1, Premium, 10, 1, 1);
+  log_registrar(T, 2, Premium, 20, 2, 2);
+  log_registrar(T, 3, Premium, 30, 3, 3);
+  log_registrar(T, 4, Premium, 40, 3, 4);
+  log_registrar(T, 5, Premium, 50, 3, 5);
+  log_registrar(T, 6, Premium, 60, 3, 6);
   ck_assert(log_media_por_classe(T, T->raiz, Premium) == 35);
   ck_assert(log_media_por_classe(T, T->raiz, Ouro) == 0);
 }
@@ -63,21 +63,74 @@ END_TEST
 
 START_TEST(test_alt_negros) {
   RBT *T = log_inicializar();
-  log_registrar(T, 1, Premium, 10, 1);
-  log_registrar(T, 2, Premium, 20, 2);
-  log_registrar(T, 3, Premium, 30, 3);
+  log_registrar(T, 1, Premium, 10, 1, 1);
+  log_registrar(T, 2, Premium, 20, 2, 1);
+  log_registrar(T, 3, Premium, 30, 3, 1);
   ck_assert(log_alt_negros(T) == 1);
 
-  log_registrar(T, 4, Premium, 40, 3);
+  log_registrar(T, 4, Premium, 40, 3, 1);
   ck_assert(log_alt_negros(T) == 2);
 
-  log_registrar(T, 5, Premium, 40, 3);
-  log_registrar(T, 6, Premium, 40, 3);
-  log_registrar(T, 7, Premium, 40, 3);
-  log_registrar(T, 8, Premium, 40, 3);
-  log_registrar(T, 9, Premium, 40, 3);
-  log_registrar(T, 10, Premium, 40, 3);
+  log_registrar(T, 5, Premium, 40, 3, 1);
+  log_registrar(T, 6, Premium, 40, 3, 1);
+  log_registrar(T, 7, Premium, 40, 3, 1);
+  log_registrar(T, 8, Premium, 40, 3, 1);
+  log_registrar(T, 9, Premium, 40, 3, 1);
+  log_registrar(T, 10, Premium, 40, 3, 1);
   ck_assert(log_alt_negros(T) == 3);
+}
+END_TEST
+
+START_TEST(test_maximo) {
+  RBT *T = log_inicializar();
+  log_registrar(T, 1, Premium, 10, 1, 1);
+  log_registrar(T, 2, Premium, 20, 2, 1);
+  log_registrar(T, 3, Premium, 30, 3, 1);
+  log_registrar(T, 4, Premium, 40, 3, 1);
+  log_registrar(T, 5, Premium, 50, 3, 10);
+  log_registrar(T, 6, Premium, 60, 3, 10);
+  log_registrar(T, 7, Premium, 70, 3, 10);
+  log_registrar(T, 8, Premium, 80, 3, 10);
+  log_registrar(T, 9, Premium, 90, 3, 10);
+  log_registrar(T, 10, Premium, 110, 3, 20);
+  Log *no = log_maximo(T);
+  ck_assert(no->chave == 110);
+}
+END_TEST
+START_TEST(test_maximo_nulo) {
+  RBT *T = log_inicializar();
+  ck_assert(log_maximo(T) == NULL);
+}
+END_TEST
+
+START_TEST(test_tubias) {
+  RBT *T = log_inicializar();
+  log_registrar(T, 1, Premium, 10, 1, 10);
+  log_registrar(T, 2, Premium, 20, 2, 10);
+  log_registrar(T, 3, Premium, 30, 3, 10);
+  log_registrar(T, 4, Premium, 40, 3, 10);
+  log_registrar(T, 5, Premium, 50, 3, 10);
+  log_registrar(T, 6, Premium, 60, 3, 10);
+  log_registrar(T, 7, Premium, 70, 3, 10);
+  log_registrar(T, 8, Premium, 80, 3, 10);
+  log_registrar(T, 9, Premium, 90, 3, 10);
+  log_registrar(T, 10, Premium, 110, 3, 20);
+  Tubias toba = log_tubias(T, Premium);
+  ck_assert(toba.clientes == 10);
+  ck_assert(toba.ops == 110);
+  ck_assert(toba.tempo_total == 560);
+}
+END_TEST
+
+START_TEST(test_tubias_nulo) {
+  RBT *T = log_inicializar();
+  log_registrar(T, 1, Premium, 10, 1, 10);
+  log_registrar(T, 2, Premium, 20, 2, 10);
+  log_registrar(T, 3, Premium, 30, 3, 10);
+  Tubias toba = log_tubias(T, Ouro);
+  ck_assert(toba.clientes == 0);
+  ck_assert(toba.ops == 0);
+  ck_assert(toba.tempo_total == 0);
 }
 END_TEST
 
@@ -94,6 +147,10 @@ Suite *logtree_suite(void) {
   tcase_add_test(tc_core, test_obter_contagem_por_classe);
   tcase_add_test(tc_core, test_media_por_classe);
   tcase_add_test(tc_core, test_alt_negros);
+  tcase_add_test(tc_core, test_maximo);
+  tcase_add_test(tc_core, test_maximo_nulo);
+  tcase_add_test(tc_core, test_tubias);
+  tcase_add_test(tc_core, test_tubias_nulo);
   suite_add_tcase(s, tc_core);
 
   return s;
